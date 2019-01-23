f231ed8ff91f82074b3d5416aaa4be29240bcc4b
patroclica <popovicm@sheridancollege.ca>
2019-01-22 21:26:39 -0500

    Added import pytest in test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -2,6 +2,7 @@
 Test the add() function of the calc selfself.
 """
 from calculator import add
+import pytest
 
 def test_two_plus_two():
     """
@@ -17,3 +18,9 @@ def test_no_parameters():
 
 def test_one_two_three():
     assert add(1, 2, 3) == 6
+
+def test_decimal_values():
+    """
+    import pytest
+    """
+    assert add (0.1, 0.1, 0.1) == pytest.approx(0.3)
